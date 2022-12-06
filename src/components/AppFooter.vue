f1cbfab9a83c0443f0cc42c78c7f46c4b7f2079d
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-12-06 10:25:09 +0100

    Aggiunge form con possibilita di registrarsi

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,24 +2,36 @@
     <section class="brend-section">
         <BrendSection/>
     </section>
+    <section class="email-subscribe">
+        <EmailSubscribe/>
+    </section>
 </template>
 
 <script>
-import BrendSection from './another-comp/main/BrendSection.vue';
+import BrendSection from './another-comp/footer/BrendSection.vue';
+import EmailSubscribe from './another-comp/footer/EmailSubscribe.vue';
+
     export default {
         name:'AppFooter',
         
         components:{
             BrendSection,
+            EmailSubscribe,
         }
     }
 </script>
 
 <style lang="scss" scoped>
 
+section{
+    padding: 60px 0px;
+}
 .brend-section{
     background-color: var(--bg_brend_section);
     padding: 60px 0px;
+}
+.email-subscribe{
+    background-color: var(--bg_email_section);
 }
 
 </style>
---
+++ src/components/another-comp/footer/EmailSubscribe.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container-70">
+        <div class="info-email" >
+            <div class="email-logo">
+                <i class="fa-regular fa-envelope"></i>
+            </div>
+            <div class="email-text">
+                <h3>Subscribe now and get special offers</h3>
+            </div>
+        </div>
+        <div class="sloat-email">
+            <form action="">
+                <input type="text" placeholder="insert your email">
+                <button class="my-button"> SEND</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'EmailSubscribe',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.container-70{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .info-email{
+        display: flex;
+        align-items: center;
+
+        .email-logo{
+            width: 70px;
+            height: 70px;
+            border-radius: 100%;
+            background-color: var(--bg_information_section);
+            text-align: center;
+            line-height: 70px;
+            color: white;
+            font-size: 30px;
+        }
+        .email-text{
+            padding: 0px 40px;
+            font-weight: bolder;
+            font-size: 27px;
+            color: var(--color_email_section);
+        }
+    }
+    .sloat-email{
+        input{
+            width:  400px;
+            padding: 5px 10px;
+            border-radius: 20px;
+        }
+        button{
+            padding: 5px 50px;
+            background-color: var(--footer_bg_button);
+            border: none;
+        }
+    }
+
+
+}
+
+</style>
